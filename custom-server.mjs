fcc9c7570d53089aa041ead9b6d34f4cc255f957
Z User <z@container>
2026-04-17 18:58:53 +0000

    f9fb8ea9-0367-4ce4-8a7b-b10933818bde

---
+++ custom-server.mjs
@@ -0,0 +1,22 @@
+import { createServer } from 'http';
+import { parse } from 'url';
+import next from 'next';
+
+const port = parseInt(process.env.PORT || '3000', 10);
+const app = next({ dev: true, hostname: '0.0.0.0', port });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer(async (req, res) => {
+    try {
+      const parsedUrl = parse(req.url, true);
+      await handle(req, res, parsedUrl);
+    } catch (err) {
+      console.error('Error occurred handling', req.url, err);
+      res.statusCode = 500;
+      res.end('internal server error');
+    }
+  }).listen(port, () => {
+    console.log(`> Ready on http://0.0.0.0:${port}`);
+  });
+});
